d603a694006d5a154f4e62a242197dfbae844802
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2024-01-18 20:01:49 +0100

    （︶^︶）

--- Persona_ej/src/Main.kt
+++ Persona_ej/src/Main.kt
@@ -2,10 +2,34 @@ import java.util.*
 
 class Persona(var nombre: String, var peso: Double, var altura: Double) {
     var imc: Double = peso / (altura * altura)
+
+    init {
+        this.imc = this.peso / (this.altura * this.altura)
+    }
+
+    fun obtenerImc() = "%.2f".format(this.imc)
+
+    fun estadoImc(): String {
+        val imc = obtenerImc().toDouble()
+        return when {
+            imc < 18.5 -> "Bajo de peso"
+            imc < 24.9 -> "Saludable"
+            imc < 29.9 -> "Sobrepeso"
+            else -> "Obesidad"
+        }
+    }
+
+    fun mostrarDesc() {
+        val imc = obtenerImc()
+        val estado = estadoImc()
+
+        println("$nombre con una altura de $altura m y un peso de $peso kg tiene un IMC de $imc, categorizado como $estado.")
+    }
+
+    fun saludar() {
+        println("Hola, Soy $nombre ")
+    }
 }
-
-
-
 
 fun main() {
     /**Constructor primario*/
@@ -25,4 +49,10 @@ fun main() {
     /**Actualizar el IMC de persona3*/
     val persona3 = Persona("Pedro", 80.0, 1.80)
     println("Persona3: Nombre - ${persona3.nombre}, IMC - ${persona3.imc}")
+
+    persona1.saludar()
+    persona2.saludar()
+
+    persona1.mostrarDesc()
+    persona2.mostrarDesc()
 }
