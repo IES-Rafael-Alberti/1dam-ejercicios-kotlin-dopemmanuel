1a49db87994736898accf7f1fc294a94a31f0e3b
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2024-01-31 18:02:58 +0100

    X_X

--- Ejercicio_4.2/src/Persona.kt
+++ Ejercicio_4.2/src/Persona.kt
@@ -5,16 +5,10 @@ class Persona(var nombre: String, var peso: Double, var altura: Double) {
         init {
         this.imc = this.peso / (this.altura * this.altura)
     }
-    constructor(nombre: String) : this(nombre,0.0, 0.0){
+    constructor(nombre: String) : this(nombre,0.0, 0.0)
 
-    }
     override fun toString(): String {
         return "Persona(nombre='$nombre', peso=$peso kg, altura=$altura m, IMC=$imc)"
     }
-
-
-
-    constructor(nombre: String, peso: Int, altura: Int) : this(nombre,peso.toDouble(), altura.toDouble()){
-
-    }
+    constructor(nombre: String, peso: Int, altura: Int) : this(nombre,peso.toDouble(), altura.toDouble())
 }
